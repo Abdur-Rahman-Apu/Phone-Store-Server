6f8de8629634bb79b9867921a0cfcb2405c1ae4f
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-02-16 07:49:56 +0600

    mongodb initial setup is done

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+const cors = require('cors');
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+
+require('dotenv').config()
+
+const port = process.env.PORT || 5000
+
+app.get('/', async (req, res) => {
+    res.send('Server is running');
+})
+
+app.listen(port, () => {
+    console.log("Server is running on port", port);
+})
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7kbtzra.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+
+    const categoriesCollection = client.db("phonesStore").collection("categories")
+
+    app.get('/categories', async (req, res) => {
+        const cursor = categoriesCollection.find({})
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+}
+
+run().catch(error => { console.log(error); })
